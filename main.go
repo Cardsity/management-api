a34b96644b022889324ef54a8394be461bb268b6
ravi0li <moritz@howtoflex.club>
2020-11-17 22:44:45 +0100

    feat: Added configuration handling

--- main.go
+++ main.go
@@ -3,15 +3,45 @@ package main
 import (
 	"github.com/Cardsity/management-api/routes"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"strconv"
 )
 
+// Handles the configuration.
+func config() {
+	// cmapi.json
+	viper.SetConfigName("cmapi")
+	viper.SetConfigType("json")
+
+	// The configuration file can be either located in /etc/cmapi or the current working directory
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("/etc/cmapi")
+
+	// Setup the environment
+	viper.SetEnvPrefix("cmapi")
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		// Environment variables are possible, too
+		log.Warn("Failed to read in configuration file: ", err)
+	}
+
+	// Default values
+	viper.SetDefault("port", 5000)
+
+	log.Debug("Successfully loaded configuration")
+}
+
 func main() {
+	config()
+
 	// Get the engine
 	router := routes.NewRouter()
 	r := router.GetEngine()
 
 	// Run the server
-	err := r.Run()
+	err := r.Run(":" + strconv.Itoa(viper.GetInt("port")))
 	if err != nil {
 		log.Panic(err)
 	}
